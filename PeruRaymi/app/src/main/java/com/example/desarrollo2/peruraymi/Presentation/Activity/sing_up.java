8888010b2847617dade3e704b49b056780851526
unknown <Niel1894>
2017-10-30 17:00:55 -0500

    3 vistas

---
+++ PeruRaymi/app/src/main/java/com/example/desarrollo2/peruraymi/Presentation/Activity/sing_up.java
@@ -0,0 +1,16 @@
+package com.example.desarrollo2.peruraymi.Presentation.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.desarrollo2.peruraymi.R;
+
+public class sing_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+        getSupportActionBar().hide();
+    }
+}
---
+++ PeruRaymi/app/src/main/java/com/example/desarrollo2/peruraymi/Presentation/Activity/splash_screen.java
@@ -0,0 +1,37 @@
+package com.example.desarrollo2.peruraymi.Presentation.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.desarrollo2.peruraymi.R;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(splash_screen.this,sing_in.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
